f3517c3387216733e30dfd0221caacf51f9882a3
shogooyedun <segunoyedun@frootify.tech>
2024-11-17 01:21:35 -0800

    created AboutUs page

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,24 @@ import Medical from "./pages/Medical"
 import WellBeing from "./pages/WellBeing"
 import Kids from "./pages/Kids"
 import Contact from "./pages/Contact"
-
-
-
-
-
-
-
-
+import AboutUs from "./pages/AboutUs"
 
 
 
 function App() {
 
   return (
-    <BrowserRouter>
+   
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/about-us" element={<AboutUs />} />
         <Route path="/education-donation" element={<Education />} />
         <Route path="/medical-donation" element={<Medical />} />
         <Route path="/kids-donation" element={<Kids />} />
         <Route path="/wellbeing-donation" element={<WellBeing />} />
       </Routes>
-    </BrowserRouter>
+  
   );
 }
 
